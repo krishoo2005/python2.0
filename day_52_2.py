e74f742e8520555bfe187ace9ab8593090a7874a
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-03-04 08:51:32 +0530

    Add files via upload

---
+++ day_52_2.py
@@ -0,0 +1,11 @@
+
+def two_sum(nums, target):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
+nums = [11,12,13,14,15]
+target = 23
+
+result = two_sum(nums, target)
+print("Indices:", result)
