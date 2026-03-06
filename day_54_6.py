e7ee8e68853b78abf29a893701ab06a328282994
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-03-06 23:56:51 +0530

    Add files via upload

---
+++ day_54_6.py
@@ -0,0 +1,15 @@
+#maximum array sum
+
+def maxSubArray(nums):
+    max_sum = nums[0]
+    current_sum = nums[0]
+
+    for i in range(1, len(nums)):
+        current_sum = max(nums[i], current_sum + nums[i])
+        max_sum = max(max_sum, current_sum)
+
+    return max_sum
+
+
+nums = [-2,1,-3,4,-1,2,1,-5,41]
+print(maxSubArray(nums))
