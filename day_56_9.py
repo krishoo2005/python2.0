766c0bc57d8c79f5bcbbebd23056806d35e1419e
Krushna Raosaheb Aghade <krushnaaghade1@gmail.com>
2026-03-14 13:34:10 +0530

    Add files via upload

---
+++ day_56_9.py
@@ -0,0 +1,15 @@
+def containsDuplicate(nums):
+
+    seen = set()
+
+    for num in nums:
+        if num in seen:
+            return True
+        seen.add(num)
+
+    return False
+
+
+nums = [1,2,3,4,1]
+
+print(containsDuplicate(nums))
